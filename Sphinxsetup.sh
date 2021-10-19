4b27cf71b8ad7da704e928ac60bea545faa766b5
TunaLobster <charliepjohnson@gmail.com>
2021-10-18 20:13:43 -0500

    Add beautifulsoup4 (this will conflict with master)

--- Sphinxsetup.sh
+++ Sphinxsetup.sh
@@ -44,7 +44,7 @@ curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py
 rm -f get-pip.py
 
-# Install sphinx, lxml for parameter parsing, ArduPilot sphinx theme, a youtube plugin, and a vimeo plugin
-python3 -m pip install sphinx==1.8.5 lxml sphinxcontrib-youtube git+https://github.com/ArduPilot/sphinx_rtd_theme.git@bd7dc2b469a3d189dcd4bb5600fecf38970b885d git+https://github.com/ArduPilot/sphinxcontrib.vimeo.git
+# Install sphinx, lxml for parameter parsing, beautifulsoup4 for the frontpage, ArduPilot sphinx theme, a youtube plugin, and a vimeo plugin
+python3 -m pip install sphinx==1.8.5 lxml sphinxcontrib-youtube beautifulsoup4 git+https://github.com/ArduPilot/sphinx_rtd_theme.git@bd7dc2b469a3d189dcd4bb5600fecf38970b885d git+https://github.com/ArduPilot/sphinxcontrib.vimeo.git
 
 echo "Setup completed successfully!"
